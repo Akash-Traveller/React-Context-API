fae4095f01077d03644fbfbe15431b52a04b0450
Akash-Traveller <akash.arkvk@gmail.com>
2023-09-28 15:10:14 +0530

    useeffect

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import NewSongForm from './NewSongForm';
 
 const SongList = () => {
+
   const [songs, setSongs] = useState([
     { title: 'almost home', id: 1 },
     { title: 'memory gospel', id: 2 },
     { title: 'this wild darkness', id: 3 }
   ]);
+
+  const [age, setAge] = useState(20);
+
   const addSong = (title) => {
     setSongs([...songs, { title, id: uuidv4() }]);
   };
+
+  useEffect(() => {
+    console.log('useEffect callback', songs);
+  }, [songs]);
+
+  useEffect(() => {
+    console.log('useEffect callback', age);
+  }, [age]);
   return (
     <div className="song-list">
       <ul>
@@ -19,6 +31,7 @@ const SongList = () => {
         })}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   );
 }

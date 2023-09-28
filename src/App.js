b9966e5f84c60177d944d60424cf5350a2930dd4
Akash-Traveller <akash.arkvk@gmail.com>
2023-09-28 12:06:45 +0530

    multiplecontext

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import Navbar from './components/NavBar';
 import BookList from './components/BookList';
 import ThemeContextProvider from './context/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './context/AuthContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle/>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
-      <nav></nav>
     </div>
   );
 }
